c0eaadaa13dbb17199f9ac2e4e134ea92d108ce5
Jose Zalacain <Jose Zalacain@AVALON>
2019-03-26 16:47:18 -0400

    Update java servlet api

--- src/main/java/org/prolobjectlink/web/servlet/HomeServlet.java
+++ src/main/java/org/prolobjectlink/web/servlet/HomeServlet.java
@@ -24,10 +24,12 @@ import java.io.PrintWriter;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet
 public class HomeServlet extends HttpServlet implements Servlet {
 
 	private static final long serialVersionUID = 7313381101418470194L;
