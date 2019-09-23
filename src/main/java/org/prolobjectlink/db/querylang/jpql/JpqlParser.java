825ba50a412f3ba4935aa1f78fb3a636cd441ac6
Jose Zalacain <Jose Zalacain@DESKTOP-P1RGQ48>
2019-09-23 08:37:39 -0400

    Change license

--- src/main/java/org/prolobjectlink/db/querylang/jpql/JpqlParser.java
+++ src/main/java/org/prolobjectlink/db/querylang/jpql/JpqlParser.java
@@ -4,11 +4,9 @@
  * %%
  * Copyright (C) 2019 Prolobjectlink Project
  * %%
- * COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Version 1.0
- * 
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Common Development and Distrubtion License as
- * published by the Sun Microsystems, either version 1.0 of the
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
@@ -16,9 +14,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
  * 
- * You should have received a copy of the Common Development and Distrubtion
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-1.0.html>.
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
 
